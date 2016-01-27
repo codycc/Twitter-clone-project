a71903334f624497d8be3d4d56498413eb8f8293
Cody Condon <codycondon50@gmail.com>
2016-01-26 20:06:22 -0500

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+ root  "pages#index"
+
+  get 'pages/home' => 'pages#home'
+
+  get 'pages/profile' => 'pages#profile'
+
+  get 'pages/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
