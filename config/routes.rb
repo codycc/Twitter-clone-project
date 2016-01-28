00b13f99fa90c9c2b39128087a18531eab3d4240
Cody Condon <codycondon50@gmail.com>
2016-01-28 17:39:43 -0500

    implemented the devise gem and working on guest and user nav bars

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
  root  "pages#index"
 
-  get 'pages/home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'pages/profile' => 'pages#profile'
+  get '/profile' => 'pages#profile'
 
-  get 'pages/explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
